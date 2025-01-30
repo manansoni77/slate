2b95655ab3390c37b582abad666b016aff096926
Manan Soni <64540033+manansoni77@users.noreply.github.com>
2025-01-30 12:16:47 +0530

    add and populate database with sample data

--- achievements/admin.py
+++ achievements/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User, StudentAchievement
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "role", "linked_student_id")
+    search_fields = ("name", "email", "role")
+    list_filter = ("role",)
+
+
+@admin.register(StudentAchievement)
+class StudentAchievementAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "school_name", "achievements")
+    search_fields = ("name", "school_name")
--- achievements/models.py
+++ achievements/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import bcrypt
+
 
 # Create your models here.
+class StudentAchievement(models.Model):
+    student_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    school_name = models.CharField(max_length=100)
+    achievements = models.TextField()
+
+
+class User(models.Model):
+    ROLE_CHOICES = [("School", "School"), ("Parent", "Parent"), ("Student", "Student")]
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    linked_student_id = models.ForeignKey(
+        StudentAchievement, null=True, blank=True, on_delete=models.SET_NULL
+    )
+
+    def set_password(self, raw_password):
+        self.password = bcrypt.hashpw(raw_password.encode(), bcrypt.gensalt()).decode()
+
+    def check_password(self, raw_password):
+        return bcrypt.checkpw(raw_password.encode(), self.password.encode())
